f7e4f9d3c3ecdedfa9daa45dcf274c0b0cee6b00
Eric <erisl@iCloud.com>
2018-12-13 09:33:02 +0800

    simplified interrupt

--- build.sh
+++ build.sh
@@ -5,9 +5,24 @@
 nasm $OS_SRC/boot/mbr.S -o $OS_BUILD/binary/mbr.bin -I $OS_SRC/boot/include/ && \
 nasm $OS_SRC/boot/loader.S -o $OS_BUILD/binary/loader.bin -I $OS_SRC/boot/include/ && \
 
+nasm -f elf $OS_SRC/kernel/kernel.S -o $OS_BUILD/object/kernel.o && \
 nasm -f elf -o $OS_BUILD/object/print.o $OS_SRC/lib/kernel/print.S && \
-gcc-elf -I $OS_SRC/lib/kernel/ -I $OS_SRC/lib/comm/ -c -o $OS_BUILD/object/main.o $OS_SRC/kernel/main.c && \
-ld-elf $OS_BUILD/object/main.o $OS_BUILD/object/print.o -Ttext 0xc0001500 -e main -o $OS_BUILD/binary/kernel.bin && \
+
+gcc-elf -I $OS_SRC/lib/kernel/ -I $OS_SRC/lib/ -c -o $OS_BUILD/object/interrupt.o $OS_SRC/kernel/interrupt.c && \
+
+gcc-elf -I $OS_SRC/lib/kernel/ -I $OS_SRC/lib/ -c -o $OS_BUILD/object/init.o $OS_SRC/kernel/init.c && \
+gcc-elf -I $OS_SRC/lib/kernel/ -I $OS_SRC/lib/ -c -o $OS_BUILD/object/main.o $OS_SRC/kernel/main.c && \
+
+
+ld-elf -o $OS_BUILD/binary/kernel.bin \
+$OS_BUILD/object/main.o \
+$OS_BUILD/object/init.o \
+$OS_BUILD/object/interrupt.o \
+$OS_BUILD/object/kernel.o \
+$OS_BUILD/object/print.o \
+-Ttext 0xc0001500 -e main && \
+ 
+
 
 dd if=$OS_BUILD/binary/mbr.bin of=$OS_RUN/c.img bs=512 count=1 conv=notrunc && \
 dd if=$OS_BUILD/binary/loader.bin of=$OS_RUN/c.img bs=512 count=4 seek=2 conv=notrunc && \
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,12 +1,15 @@
 #include "print.h"
+#include "init.h"
 void main(void) {
    put_str("I am kernel\n"); 
-  
-   put_int(9);
+   put_int(17);
    put_char('\n');
    put_int(0x00021a3f);
    put_char('\n'); 
-   
-   while(1);
-}
+   // 测试 print
 
+   init_all();
+   asm volatile("sti");	     // 为演示中断处理, 在此临时开中断
+ 
+   while(1);
+} 
